20b6ee024135566df1ab57920900e6ca7a07c1a8
chiaramery <meringolachiara@gmail.com>
2022-12-16 16:46:18 +0100

    Dati stampati in pagina

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -15,7 +15,7 @@ class PageController extends Controller
     public function index()
     {
         $movies = Movie::all();
-        dd($movies);
+        // dd($movies);
         return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <section>
+
+        <h2 class="text-center">Film:</h2>
+        <ul class="row row-cols-5">
+            @foreach ($movies as $movie)
+                <li class="card col">
+                    <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                    <h3>Titolo: {{ $movie->title }}</h3>
+                    <p>Titolo originale: {{ $movie->original_title }}</p>
+                    <span>Voto: {{ $movie->vote }}</span>
+                </li>
+            @endforeach
+        </ul>
+    </section>
 @endsection
