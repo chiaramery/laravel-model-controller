1c639f437fbe0cabe0f8a6d86f572a52aba875ab
chiaramery <meringolachiara@gmail.com>
2022-12-16 16:25:37 +0100

    Connessa al database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Guest\Movie;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Movie::class, 'index'])->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
